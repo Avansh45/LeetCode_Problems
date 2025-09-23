5ffd040019e09116a880aeac7740788900ca2d0d
Avansh Chadgal <avanshchadgalmh25@gmail.com>
2025-09-23 18:40:00 +0530

    Arrays Problem 66

---
+++ Arrays_Prob_66.java
@@ -0,0 +1,25 @@
+// Problem Statement:  Arrays Problem Number 66
+
+/* You are given a large integer represented as an integer array digits,
+ where each digits[i] is the ith digit of the integer.
+The digits are ordered from most significant to least significant in left-to-right order. 
+The large integer does not contain any leading 0's.
+Increment the large integer by one and return the resulting array of digits. */
+ 
+
+public class Arrays_Prob_66 {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+          for(int i=len-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+          }
+        int []newDigits = new int[len+1];
+        newDigits[0]=1;
+
+        return newDigits;
+    }
+}
