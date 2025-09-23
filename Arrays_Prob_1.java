c2ba16bc2ba2e51c87a49d4f2eebd78e55615c9f
Avansh Chadgal <avanshchadgalmh25@gmail.com>
2025-09-23 18:05:13 +0530

    Arrays Problem 1

---
+++ Arrays_Prob_1.java
@@ -0,0 +1,21 @@
+// Problem Statement:  Arrays Problem Number 1
+// Given an array of integers nums and an integer target, 
+// return indices of the two numbers such that they add up to target.
+import java.util.Arrays;
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length-1; i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                return new int[]{i,j};
+            }
+            }
+            
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[]res = twoSum(new int[]{1,2,3,4,5,6},11);
+        System.out.println(Arrays.toString(res));
+    }
+}
