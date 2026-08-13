e0e765d357f6c257fc3b36e29afff7805fc40964
Avansh Chadgal <avanshchadgalmh25@gmail.com>
2026-08-13 19:28:54 +0530

    Time: 3 ms (86.08%) | Memory: 43.3 MB (58.01%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch == '[' || ch =='{'){
+                stack.push(ch);
+            }
+            else if(ch==')' || ch == ']' || ch =='}'){
+                if(stack.isEmpty()) return false;
+                char poppedChar = stack.pop();
+                if( poppedChar == '(' && s.charAt(i)!=')' 
+                || poppedChar == '[' && s.charAt(i)!=']'
+                || poppedChar == '{' && s.charAt(i)!='}')
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
